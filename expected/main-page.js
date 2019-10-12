ecf7f00e9c343091e52a0d95344c07a9b981ebe2
Foryoushka <egboytsova@gmail.com>
2019-10-12 01:57:55 -0400

    first commit

---
+++ expected/main-page.js
@@ -0,0 +1,6 @@
+export default {
+    mainPageTitle: 'Redefining Healthcare Communication with EHR Interoperability | Forcura',
+    h1Text: 'Great Healthcare Is Built On Simple, Secure Communication.',
+
+
+}
---
+++ selectors/main-page.js
@@ -0,0 +1,9 @@
+export default {
+    h1: '//h1',
+    mainMenuBar: '//div[@class="nb2018-navigation-wrap"]//div[@aria-label="Navigation Menu"]',
+    logoHeader: '//div[@class="nb2018-logo-wrap logo-white"]//img[@class="nb2018-logo"]',
+    platformMenuElement: '//div[@class="nb2018-navigation-wrap"]//a[contains(text(),"Platform")]',
+    documentWorkflow: '//div[@class="nb2018-navigation-wrap"]//a[contains(text(),"Document Workflow")]',
+    careCoordination: '//div[@class="nb2018-navigation-wrap"]//a[contains(text(),"Care Coordination")]'
+
+}
